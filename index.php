e9260b23f53005d1a9b910378fa43a7d3f16320f
nlshad <nnishadt@gmail.com>
2026-05-18 18:41:26 +0530

    Create root index.php redirect to public/index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+// index.php (Root Redirection)
+// Automatically forwards visitors from https://ppllive.online/ to the live spectator dashboard!
+
+header("Location: public/index.php");
+exit;
+?>
